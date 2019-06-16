c81ce700ac83c4500704f50306b45894a06d5a57
Jiahui Tang <tangjiahui26@gmail.com>
2019-06-15 21:50:20 -0700

    Update CC Exercises

--- BitManipulation/Insertion/main.cpp
+++ BitManipulation/Insertion/main.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
+int updateBits(int n, int m, int i, int j){
+    int allOnes = ~0;
+
+    int right = allOnes << (j+1);
+    int left = (1 << i) - 1;
+
+    int mask = right | left;
+
+    int n_cleared = n & mask;
+    int m_shifted = m << i;
+
+    return n_cleared | m_shifted;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int n = 1024;
+    int m = 19;
+    int i = 2;
+    int j = 6;
+
+    int result = updateBits(n, m, i, j);
+
+    // output should be 1100
+    cout << "Output is: " << result << endl;
+
     return 0;
 }
