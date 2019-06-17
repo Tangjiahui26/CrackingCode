1cfa7b19eb949833b6785f668e30300cbee99aa3
Jiahui Tang <tangjiahui26@gmail.com>
2019-06-16 23:32:02 -0700

    Update CC Exercises

--- BitManipulation/BinaryToString/main.cpp
+++ BitManipulation/BinaryToString/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+string binaryToString(double num){
+    if (num >=1 || num <= 0) {
+        return "ERROR";
+    }
+
+    string result;
+    result.push_back('.');
+    while (num > 0) {
+        if (result.length() >= 32) {
+            return "ERROR";
+        }
+
+        double r = num * 2;
+        if(r >= 1) {
+            result.push_back('1');
+            num = r - 1;
+        } else {
+            result.push_back('0');
+            num = r;
+        }
+    }
+    return result;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    string s = binaryToString(0.6666259765625);
+    cout << "Binary Representation: " << s << endl;
     return 0;
 }
