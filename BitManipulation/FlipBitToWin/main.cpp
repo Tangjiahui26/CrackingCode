31bfbe855dfde3cda02cea3a769d376f054cb90e
Jiahui Tang <tangjiahui26@gmail.com>
2019-06-17 21:08:17 -0700

    Update CC Exercises

--- BitManipulation/FlipBitToWin/main.cpp
+++ BitManipulation/FlipBitToWin/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+int flipOneBit(int num){
+    if (~num == 0) {
+        return sizeof(num) * 8;
+    }
+
+    int currentLength = 0;
+    int previousLength = 0;
+    int maxLength = 1;
+
+    while (num != 0) {
+        if ((num & 1) == 1) {
+            currentLength++;
+        } else if ((num & 1) == 0) {
+            // next bit after a '0'
+            previousLength = (num & 2) == 0 ? 0 : currentLength;
+            currentLength = 0;
+        }
+        maxLength = max(previousLength + currentLength + 1, maxLength);
+        // logical right shift by 1 bit
+        num = (int)((unsigned int) num >> 1);
+    }
+    return maxLength;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int test = 1775;
+    cout << "Flip one bit to win: " << flipOneBit(test) << endl;
     return 0;
 }
