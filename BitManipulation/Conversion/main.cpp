1290b9ef9a0ae6bb26084f0e9172e9e6bb3108aa
Jiahui Tang <tangjiahui26@gmail.com>
2019-06-19 12:37:50 -0700

    Update CC Exercises

--- BitManipulation/Conversion/main.cpp
+++ BitManipulation/Conversion/main.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+int conversion(int a, int b) {
+    int count = 0;
+    for (int c = a ^ b; c != 0; c = c >> 1) {
+        count += c & 1;
+    }
+    return count;
+}
+
+int conversion2(int a, int b) {
+    int count = 0;
+    for (int c = a ^ b; c != 0; c = c & (c-1)) {
+        count++;
+    }
+    return count;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int a = 29;
+    int b = 15;
+    cout << "Conversion: " << conversion(a, b) << endl;
+    cout << "Conversion: " << conversion2(a, b) << endl;
     return 0;
 }
