00287a3eb69bac8b27e3ae90f6b5d9e6c20d0c76
Jiahui Tang <tangjiahui26@gmail.com>
2019-07-05 20:30:41 -0700

    Update CC Exercises

--- RecursionAndDP/PowerSet/main.cpp
+++ RecursionAndDP/PowerSet/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <algorithm>
+#include <set>
 
 using namespace std;
 
+set<set<int>> powerSet(const set<int>& origin_set){
+    set<set<int>> sets;
+    sets.insert(origin_set);
+
+    for (int i : origin_set){
+        set<int> origin_set_copy(origin_set);
+        origin_set_copy.erase(i);
+        for (const set<int>& subset: powerSet(origin_set_copy)) {
+            sets.insert(subset);
+        }
+    }
+    return sets;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    set<int> test = {0, 1, 2, 3};
+    set<set<int>> results = powerSet(test);
+    for (const set<int> & s : results){
+        cout << "{ ";
+        for (int i : s) {
+            cout << i << " ";
+        }
+        cout << "}" << endl;
+    }
     return 0;
 }
