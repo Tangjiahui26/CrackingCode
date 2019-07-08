f30e194fd6a4c05e6866ca79fda0717b3d2bcd6c
Jiahui Tang <tangjiahui26@gmail.com>
2019-07-08 15:20:16 -0700

    Update CC Exercises

--- RecursionAndDP/PermutationsWithOrWithoutDups/main.cpp
+++ RecursionAndDP/PermutationsWithOrWithoutDups/main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
 
 using namespace std;
 
+// without duplicates
+
+vector<string> getPermutations(string str){
+    vector<string> permutations;
+
+    if (str.empty()){
+        permutations.push_back(str);
+        return permutations;
+    }
+
+    char last_char = str.back();
+    str.pop_back();
+
+    for (const string& permutation : getPermutations(str)){
+        for (size_t i = 0; i <= permutation.length(); i++){
+            string permutation_copy = permutation;
+            permutation_copy.insert(i, 1, last_char);
+            permutations.push_back(permutation_copy);
+        }
+    }
+
+    return permutations;
+}
+
+// with duplicates -> no duplicates in output permutations
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    string test = "abc";
+    for(const string& str : getPermutations(test)) {
+        cout << str << endl;
+    }
     return 0;
 }
