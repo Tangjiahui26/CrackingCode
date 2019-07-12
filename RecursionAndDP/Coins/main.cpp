6ac67b2d084b98d787632c1391a87c8a1b1c5e7b
Jiahui Tang <tangjiahui26@gmail.com>
2019-07-12 16:55:15 -0700

    Update CC Exercises

--- RecursionAndDP/Coins/main.cpp
+++ RecursionAndDP/Coins/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
+// without using DP
+int makeChange(int amount, const vector<int> & denoms, int index){
+    if (index == denoms.size() - 1) {
+        return 1;
+    }
+
+    int denoms_amount = denoms[index];
+    int ways = 0;
+    for (int i = 0; i <= amount; i += denoms_amount){
+        int remaining = amount - i;
+        ways += makeChange(remaining, denoms, index + 1);
+    }
+    return ways;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    const vector<int> coins = {25, 10, 5, 1};
+    for (int i = 1; i <= 25; i++){
+        cout << "Ways to represent " << i << " cents: " << makeChangeDP(i, coins) << endl;
+    }
     return 0;
 }
