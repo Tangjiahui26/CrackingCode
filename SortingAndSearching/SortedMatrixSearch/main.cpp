3bd2de76d5ce063a190bc854452ebac9635aef1e
Jiahui Tang <tangjiahui26@gmail.com>
2019-07-22 21:48:11 -0700

    Update CC Exercises

--- SortingAndSearching/SortedMatrixSearch/main.cpp
+++ SortingAndSearching/SortedMatrixSearch/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
+bool sortedMatrixSearch(const vector<vector<int>> matrix, const int target){
+    if (matrix.empty() || matrix[0].empty()) {
+        return false;
+    }
+
+    int row = 0;
+    int col = matrix[0].size() - 1;
+    while (row < matrix.size() && col >= 0){
+        if (matrix[row][col] == target){
+            return true;
+        } else if (matrix[row][col] > target){
+            col--;
+        } else {
+            row++;
+        }
+    }
+    return false;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    vector<vector<int>> test = {{15, 20, 40, 85},
+                                {20, 35, 80, 95},
+                                {30, 55, 95, 105},
+                                {40, 80, 100, 200}};
+    int target = 80;
+    string result = sortedMatrixSearch(test, target) ? "true" : "false";
+    cout << "Given target = " << target << ", return " << result;
     return 0;
 }
