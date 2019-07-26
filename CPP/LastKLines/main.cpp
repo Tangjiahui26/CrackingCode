9d8d59fa7551f1a38a4f8b58c90dc2e337ea46cb
Jiahui Tang <tangjiahui26@gmail.com>
2019-07-25 21:26:04 -0700

    Update CC Exercises

--- CPP/LastKLines/main.cpp
+++ CPP/LastKLines/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <istream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+#include <cmath>
 
 using namespace std;
 
+void getLastKLines(char* filename, const int K){
+    ifstream file (filename);
+    string Lines[K];
+    int size = 0;
+
+    // read file line by line until an EOF
+    while(file.peek() != EOF){
+        getline(file, Lines[size % K]);
+        size++;
+    }
+
+    // get the start (oldest line) and the line count
+    int start = size > K ? (size % K) : 0;
+    int count = min(size, K);
+
+    for (int i = 0; i < count; i++){
+        cout << Lines[(start + i) % K] << endl;
+    }
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    char * name = "/Users/tangjiahui/Desktop/CC/CPP/LastKLines/test.txt";
+    getLastKLines(name, 5);
     return 0;
 }
