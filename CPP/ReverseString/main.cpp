ffbbc96913190fd3178a1357da824b7e1a2b051b
Jiahui Tang <tangjiahui26@gmail.com>
2019-07-26 14:25:30 -0700

    Update CC Exercises

--- CPP/ReverseString/main.cpp
+++ CPP/ReverseString/main.cpp
@@ -2,8 +2,30 @@
 
 using namespace std;
 
+void reverseString(char * str){
+    char* end = str;
+    char temp;
+    if (str){
+        while(*end){
+            ++end;
+        }
+        // last char is null
+        --end;
+    }
+
+    // swap characters from start of string with the end of the string untik
+    // the pointers meet in middle
+    while(str < end){
+        temp = *str;
+        *str++ = *end;
+        *end-- = temp;
+    }
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    char s[] = "testString";
+    reverseString(s);
+    cout << s << endl;
     return 0;
 }
