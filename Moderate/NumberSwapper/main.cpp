f5d6b48a508cc42beb518a6dac305f58e0035973
Jiahui Tang <tangjiahui26@gmail.com>
2019-07-29 16:37:08 -0700

    Update CC Exercises

--- Moderate/NumberSwapper/main.cpp
+++ Moderate/NumberSwapper/main.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
+void swappingByDiff(int &a, int &b){
+    // we assume that a is greater than b
+    a = a - b; // diff
+    b = a + b;
+    a = b - a;
+}
+
+// more general, we can use XOR to swap two data
+
+template<class T>
+void swappingXOR(T &a, T &b){
+    a = a^b;
+    b = a^b; // 1 -> flips ; 0 -> keeps the same
+    a = a^b;
+}
 int main()
 {
-    cout << "Hello World!" << endl;
+    int test_a = 10;
+    int test_b = 5;
+    swappingByDiff(test_a, test_b);
+    cout << "After Swapping: a is "<< test_a << ", b is " << test_b << endl;
+
+    long a = 133;
+    long b = 2;
+    swappingXOR<long>(a, b);
+    cout << "After Swapping: a is "<< a << ", b is " << b << endl;
     return 0;
 }
