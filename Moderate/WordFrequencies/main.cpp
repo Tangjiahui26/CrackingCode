2432f70fe1e65dffabd656ee18ae07331e59f164
Jiahui Tang <tangjiahui26@gmail.com>
2019-07-30 21:18:25 -0700

    Update CC Exercises

--- Moderate/WordFrequencies/main.cpp
+++ Moderate/WordFrequencies/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
+#include <fstream>
+#include <map>
+#include <algorithm>
+#include <string>
 
 using namespace std;
 
-int main()
+typedef map<string, int> StrIntMap;
+
+void countWords(istream& in, StrIntMap& words){
+    string s;
+    while (in >> s){
+        ++words[s];
+    }
+}
+
+int main(int argc, char** argv)
 {
-    cout << "Hello World!" << endl;
+    if (argc < 2){
+        return(EXIT_FAILURE);
+    }
+    ifstream in(argv[1]);
+
+    if (!in){
+        return(EXIT_FAILURE);
+    }
+
+    StrIntMap w;
+    countWords(in, w);
+
+    for (StrIntMap::iterator p = w.begin(); p != w.end(); ++p){
+        cout << p->first <<" occurred " << p->second << " times" << endl;
+    }
     return 0;
 }
