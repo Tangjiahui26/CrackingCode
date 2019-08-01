23aab7d425b1896f1cdb8f6686c8bf627cb3fc68
Jiahui Tang <tangjiahui26@gmail.com>
2019-08-01 15:56:20 -0700

    Update CC Exercises

--- Moderate/FactorialZeros/main.cpp
+++ Moderate/FactorialZeros/main.cpp
@@ -2,8 +2,47 @@
 
 using namespace std;
 
+// The basic solution to this coding question is to
+// count the pairs of multiples of 2 and 5
+
+// since we can find that 2s are always more than or equal
+// the number of 5s, so we can just calucate the number of 5s
+
+// special cases: 25, 125
+
+// approach 1: factorsOf5 + number iteration
+
+int factorOf5(int i){
+    int count = 0;
+    while (i % 5 == 0){
+        count++;
+        i = i / 5;
+    }
+    return count;
+}
+
+int factorialZeros(int number){
+    int count = 0;
+    for (int i = 2; i <= number; i++){
+        count += factorOf5(i);
+    }
+    return count;
+}
+
+// approach 2: just one iteration
+
+int factorialZerosSimplification(int number){
+    int count = 0;
+    for (int i = 5; number / i >= 1; i *= 5){
+        count += number / i;
+    }
+    return count;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int test = 100;
+    cout << "Result: " << factorialZeros(test) << endl;
+    cout << "Result: " << factorialZerosSimplification(test) << endl;
     return 0;
 }
