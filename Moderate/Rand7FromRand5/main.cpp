c64519735016f5a3d98b86a028f1840d95293d7f
Jiahui Tang <tangjiahui26@gmail.com>
2019-08-12 21:41:53 -0700

    Update CC Exercises

--- Moderate/Rand7FromRand5/main.cpp
+++ Moderate/Rand7FromRand5/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
+int random5(){
+    return rand() % 5;
+}
+
+// Approach 1: 5 * rand(5) + rand(5)
+int random7(){
+    while(true){
+        int num = 5 * random5() + random5();
+        if (num < 21){
+            return num % 7;
+        }
+    }
+}
+
+// Approach 2: 2 * rand(5) + rand(5)
+int rand7(){
+    while(true){
+        int r1 = 2 * random5();
+        int r2 = random5();
+        if (r2 != 4){
+            // get 0 and 1
+            int rand2 = r2 % 2;
+            int num = r1 + rand2;
+            if (num < 7){
+                return num;
+            }
+        }
+    }
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    cout << random7() << endl;
     return 0;
 }
