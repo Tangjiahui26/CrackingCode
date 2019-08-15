e4c6bc8391b61e83e92831b9ddffa4d8c1ff1993
Jiahui Tang <tangjiahui26@gmail.com>
2019-08-14 20:12:16 -0700

    Update CC Exercises

--- Hard/RandomSet/main.cpp
+++ Hard/RandomSet/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <random>
+#include <vector>
 
 using namespace std;
 
+// similar to question(shuffle), here we need a generator to get random numbers in range
+
+int generateRandom(int min, int max){
+    random_device rd;
+    mt19937 eng(rd()); // seed the generator
+    uniform_int_distribution<> dis(min, max);
+    return dis(eng);
+}
+
+vector<int> getRandomSet(vector<int> arr, int m){
+    vector<int> res(m);
+    for(int i = 0; i < m; i++){
+        res[i] = arr[i];
+    }
+
+    for(int i = m; i < arr.size(); i++){
+        int k = generateRandom(0, i);
+        if (k < m){
+            res[k] = arr[i];
+        }
+    }
+    return res;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    vector<int> test = {1, 2, 3, 4, 5, 6};
+    vector<int> res = getRandomSet(test, 3);
+    for (auto i : res){
+        cout << i << " ";
+    }
+    cout << endl;
     return 0;
 }
