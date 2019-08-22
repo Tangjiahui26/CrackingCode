5efe746d90476dec43cbfd3acd8428f389031db5
Jiahui Tang <tangjiahui26@gmail.com>
2019-08-22 16:23:42 -0700

    Update CC Exercises

--- Others-LeetCode&Geeksforgeeks/CaseInsensitiveStringComparison/main.cpp
+++ Others-LeetCode&Geeksforgeeks/CaseInsensitiveStringComparison/main.cpp
@@ -1,9 +1,49 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <cctype>
 
 using namespace std;
 
+// C++ 11 Approach1: Using transfrom to all lowercase
+
+int caseInsensitiveMatch(string s1, string s2){
+    transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+    transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+
+    if (s1.compare(s2) == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+// C++ 11 Approach2: equal() with self-define compare function or lambda function
+
+bool caseInsensitiveMatchCpp11(string &s1, string &s2){
+    return ((s1.size() == s2.size()) && (equal(s1.begin(), s1.end(), s2.begin(), [](char &c1, char & c2){
+        return ((c1 == c2) || (::toupper(c1) == ::toupper(c2)));})));
+}
+
+// C++ Approcah3: isequal()
+// returns true if both strings are equal in case insensitive manner
+
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    string s1, s2;
+    s1 = "Hello World";
+    s2 = "HEllO WORld";
+    if (caseInsensitiveMatch(s1, s2)){
+        cout << "Strings are same" << endl;
+    } else {
+        cout << "STrings are not same" << endl;
+    }
+
+    if (caseInsensitiveMatchCpp11(s1, s2)){
+        cout << "Strings are same" << endl;
+    } else {
+        cout << "STrings are not same" << endl;
+    }
+
     return 0;
 }
---
+++ Others-LeetCode&Geeksforgeeks/DeleteNodeOfLinkedListOnlyGivenAPointer/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// Problem description:
+/*
+ * Given only a pointer to a node
+ * to be deleted in a singly linked
+ *  list, how do you delete it?
+ */
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
