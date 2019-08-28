9bd2a377f626e081c53cf7fe2d872a7e60d8f85f
Jiahui Tang <tangjiahui26@gmail.com>
2019-08-28 11:22:30 -0700

    Update CC Exercises

--- Others-LeetCode&Geeksforgeeks/MaxDepthOfBalancedParenthesis/main.cpp
+++ Others-LeetCode&Geeksforgeeks/MaxDepthOfBalancedParenthesis/main.cpp
@@ -2,8 +2,43 @@
 
 using namespace std;
 
+int findMaxDepth(string str){
+    int current_max = 0; // current count
+    int max = 0;
+    int n = str.length();
+
+    for (int i = 0; i < n; i++){
+        if (str[i] == '('){
+            current_max++;
+            if (current_max > max){
+                max = current_max;
+            }
+        } else if (str[i] == ')'){
+            if (current_max > 0){
+                current_max--;
+            } else {
+                return -1;
+            }
+        }
+    }
+
+    if (current_max != 0){
+        return -1;
+    }
+
+    return max;
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    string s1 = "b)(c)()";
+    cout << findMaxDepth(s1) << endl;
+
+    string s2 = "";
+    cout << findMaxDepth(s2) << endl;
+
+    string s3 = "(((X))(((Y))))";
+    cout << findMaxDepth(s3) << endl;
+
     return 0;
 }
